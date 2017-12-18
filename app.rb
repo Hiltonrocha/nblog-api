f085ac5714b928d7cae64134677f492c345ce06d
Hiltonrocha <hilton-bruno-santos@hotmail.com>
2017-12-18 16:31:02 -0200

    Mudancas no mongoid.ylm

--- app.rb
+++ app.rb
@@ -7,6 +7,7 @@ require_relative 'endpoints/posts'
 
 # Rota padrão
 get '/' do
+  ENV['RACK_ENV'] = 'development'
   @env = ENV['RACK_ENV'].to_s.upcase
   "Welcome to nBlog API v2.5 | Environment: #{@env}."
 end
